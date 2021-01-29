13d58474c32c5573f2b6937f432b7b021a0cfd40
haotingfang <haotingfang@souche.com>
2021-01-29 15:44:09 +0800

    logo更新

--- richhao-ui/src/views/login.vue
+++ richhao-ui/src/views/login.vue
@@ -97,7 +97,7 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../assets/image/login-background.jpg");
+  background-image: url("../assets/image/login-background.jpeg");
   background-size: cover;
 }
 .title {
